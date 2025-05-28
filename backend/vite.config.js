e7f88e3491266caa2d91287f34b05deefdccef33
davidvillon04 <davidv04@vt.edu>
2025-05-28 15:34:49 -0400

    Update vite.config.js

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "https://localhost:5050", // your backend
+        target: "https://test-spotify-site.local:5050", // your backend
         changeOrigin: true,
         secure: false, // needed for self-signed HTTPS certs
       },
