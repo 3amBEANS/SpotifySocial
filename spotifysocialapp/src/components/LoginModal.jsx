8e63d872ccbf7c33ade4e2c2b5885ee08d685b9b
davidvillon04 <davidv04@vt.edu>
2025-05-27 22:22:56 -0400

    User will be met with login modal if not signed in and tries to access other pages

--- spotifysocialapp/src/components/LoginModal.jsx
+++ spotifysocialapp/src/components/LoginModal.jsx
@@ -1,5 +1,7 @@
 import {
-  Flex,
+  Modal,
+  ModalOverlay,
+  ModalContent,
   Box,
   HStack,
   Circle,
@@ -9,47 +11,57 @@ import {
   Button,
   Divider,
 } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import SpotifyLogo from "../assets/spotify.png";
 import "../styles/loginPage.css";
 
-export default function LoginModal() {
+export default function LoginModal({ isOpen, onClose }) {
   return (
-    <Flex className="login-page">
-          <Box className="login-container">
-            {/* Header bar */}
-            <Box className="login-header">
-              <HStack>
-                <Circle className="login-header-circle" />
-                <Text className="login-header-text">Welcome to Spotify Connect</Text>
-              </HStack>
-            </Box>
-    
-            {/* Content */}
-            <VStack className="login-content">
-              {/* Sign-in section */}
-              <VStack className="login-section">
-                <Heading className="login-heading">Sign In with Spotify</Heading>
-                <Text className="login-text">
-                  Access your favorite music and playlists by signing in with your Spotify account.
-                </Text>
-                <Button
-                  as="a"
-                  href="https://test-spotify-site.local:5050/login"
-                  className="login-button"
-                >
-                  Sign in with Spotify
-                </Button>
-              </VStack>
-    
-              <Divider className="login-divider" />
-    
-              {/* Register section */}
-              <VStack className="login-section">
-                <Heading className="login-heading">Don't have a Spotify Account?</Heading>
-                <Text className="login-text">Creating a Spotify account is free!</Text>
-                <Button className="login-button">Register</Button>
-              </VStack>
-            </VStack>
+    <Modal isCentered isOpen={isOpen} onClose={onClose} size="sm">
+      <ModalOverlay />
+      <ModalContent>
+        <Box className="login-container">
+          {/* Header bar */}
+          <Box className="login-header">
+            <HStack>
+              <Circle className="login-header-circle" />
+              <Text className="login-header-text">Welcome to Spotify Connect</Text>
+            </HStack>
           </Box>
-        </Flex>
-  )
+
+          {/* Content */}
+          <VStack className="login-content">
+            {/* Sign-in section */}
+            <VStack className="login-section">
+              <Heading className="login-heading">
+                Sign In with <img src={SpotifyLogo} alt="Spotify logo" className="spotify-logo" />
+                Spotify
+              </Heading>
+              <Text className="login-text">
+                Access your favorite music and playlists by signing in with your Spotify account.
+              </Text>
+              <Button
+                as="a"
+                href="https://test-spotify-site.local:5050/login"
+                className="login-button"
+              >
+                Sign in with Spotify <ExternalLinkIcon />
+              </Button>
+            </VStack>
+
+            <Divider className="login-divider" />
+
+            {/* Register section */}
+            <VStack className="login-section">
+              <Heading className="login-heading">Don't have a Spotify Account?</Heading>
+              <Text className="login-text">Creating a Spotify account is free!</Text>
+              <Button className="login-button">
+                Register <ExternalLinkIcon />
+              </Button>
+            </VStack>
+          </VStack>
+        </Box>
+      </ModalContent>
+    </Modal>
+  );
 }
