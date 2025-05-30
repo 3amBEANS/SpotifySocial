e093466bf0067b4dfac1c34af7201618ab4d99c7
davidvillon04 <davidv04@vt.edu>
2025-05-29 20:44:35 -0400

    Create index.jsx

---
+++ spotifysocialapp/src/assets/artists/index.jsx
@@ -0,0 +1,43 @@
+import _1 from "./1.jpg";
+import _2 from "./2.jpg";
+import _3 from "./3.jpg";
+import _4 from "./4.jpg";
+import _5 from "./5.jpg";
+import _6 from "./6.jpg";
+import _7 from "./7.jpg";
+import _8 from "./8.jpg";
+import _9 from "./9.jpg";
+import _10 from "./10.jpg";
+import _11 from "./11.jpg";
+import _12 from "./12.jpg";
+import _13 from "./13.jpg";
+import _14 from "./14.jpg";
+import _15 from "./15.jpg";
+import _16 from "./16.jpg";
+import _17 from "./17.jpg";
+import _18 from "./18.jpg";
+import _19 from "./19.jpg";
+import _20 from "./20.jpg";
+
+export {
+  _1,
+  _2,
+  _3,
+  _4,
+  _5,
+  _6,
+  _7,
+  _8,
+  _9,
+  _10,
+  _11,
+  _12,
+  _13,
+  _14,
+  _15,
+  _16,
+  _17,
+  _18,
+  _19,
+  _20,
+};
